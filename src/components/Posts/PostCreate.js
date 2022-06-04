42b78ce6c3c0bfceab43551b75473eb620044b2b
Brian Ng <ngshunchiang@hotmail.com>
2022-06-04 20:52:48 +0800

    fix post create

--- src/components/Posts/PostCreate.js
+++ src/components/Posts/PostCreate.js
@@ -1,22 +1,17 @@
-import {Grow, Grid} from '@mui/material';
+import {Grid} from '@mui/material';
 import React, {useState} from 'react';
-import Posts from './Posts';
 import Form from '../Form/Form';
 
 function PostCreate() {
     const [currentId, setcurrentId] = useState(0);
    
     return (
-        <Grow in>
-            <Grid container direction={{xs:'column-reverse', sm:'column-reverse', md: 'row'}} p={1} justify="space-between" alignItems="stretch" spacing={3}>
-                <Grid item xs={12} sm={6} md={3}>
-                    <Form currentId={currentId} setcurrentId={setcurrentId}/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={9}>
-                    {/* <Posts setcurrentId={setcurrentId}/> */}
-                </Grid>
+        <Grid container p={1} justifyContent="space-around" alignItems="center" spacing={1}>
+            <Grid item xs={12} sm={6} md={3}>
+                <Form currentId={currentId} setcurrentId={setcurrentId}/>
             </Grid>
-        </Grow>
+        </Grid>
+    
         
     )
 }
